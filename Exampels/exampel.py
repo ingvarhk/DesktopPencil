f717fe6c73fc5858519a6ba681cecab4a93b140a
HikBit <ingvar@hahnkristensen.dk>
2020-04-09 16:58:29 +0200

    adding new exampel

--- Exampels/exampel.py
+++ Exampels/exampel.py
@@ -1,7 +1,7 @@
 import DesktopPencil
 
-#frog_pixels = DesktopPencil.load("frog.png", 50, 1005, rotation=0)
-#DesktopPencil.draw(frog_pixels, 100000)
+frog_pixels = DesktopPencil.load("frog.png", 50, 1005, rotation=0)
+DesktopPencil.draw(frog_pixels, 100000)
 
-cat_pixels = DesktopPencil.load("epic_glasses.png", 500, 300, rotation=25, size=(500,500))
-DesktopPencil.draw(cat_pixels, 100000)
+#cat_pixels = DesktopPencil.load("epic_glasses.png", 500, 300, rotation=25, size=(500,500))
+#DesktopPencil.draw(cat_pixels, 100000)
